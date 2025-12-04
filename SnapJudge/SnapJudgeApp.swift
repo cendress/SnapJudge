daf1136dc6b5738adc2e5ff2f481c1cc3822e18f
cendress <95492688+cendress@users.noreply.github.com>
2025-12-04 15:24:11 -0500

    Initial Commit

---
+++ SnapJudge/SnapJudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapJudgeApp.swift
+//  SnapJudge
+//
+//  Created by Christopher Endress on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapJudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
